932e1cebf501cc39352cb2c3fe58898abd9e78cb
John Mark Acala <johnmarkacala@yahoo.com>
2025-02-25 08:21:42 +0800

    clean up and code enhancement

--- scrapy_prometheus/scrapy_prometheus.py
+++ scrapy_prometheus/scrapy_prometheus.py
@@ -1,181 +1,98 @@
-import os
-import functools
 import socket
-from collections import defaultdict
 
 import prometheus_client
 from scrapy import statscollectors, signals
-
 from .scrapy_prometheus_endpoint import ScrapyPrometheusWebServiceMixin
 
 METRIC_COUNTER = prometheus_client.Counter
 METRIC_GAUGE = prometheus_client.Gauge
 
-
-def push_to_gateway(pushgateway, registry, method='POST', timeout=5, job='scrapy', grouping_key=None):
-    if method.upper() == 'POST':
-        push = prometheus_client.pushadd_to_gateway
-    else:
-        push = prometheus_client.push_to_gateway
-
-    return push(pushgateway, job=job, grouping_key=grouping_key, timeout=timeout, registry=registry)
-
-
-class InvalidMetricType(TypeError):
-    pass
-
-
-def _forced_spider(func):
-    @functools.wraps(func)
-    def wrapper(self, *args, **kwargs):
-        """
-        :param PrometheusStatsCollector self:
-        """
-        spider = getattr(self, '_forced_spider', None)
-        if spider:
-            kwargs.setdefault('spider', spider)
-        return func(self, *args, **kwargs)
-
-    return wrapper
-
-
+def push_to_gateway(pushgateway, registry, method, timeout, job, grouping_key):
+    push_func = prometheus_client.pushadd_to_gateway if method.upper() == 'POST' else prometheus_client.push_to_gateway
+    return push_func(pushgateway, job=job, grouping_key=grouping_key, timeout=timeout, registry=registry)
 class PrometheusStatsCollector(ScrapyPrometheusWebServiceMixin, statscollectors.StatsCollector):
     def __init__(self, crawler, **kwargs):
-        """
-        :param scrapy.crawler.Crawler crawler:
-        """
         self.crawler = crawler
-        self.registries = defaultdict(lambda: prometheus_client.CollectorRegistry())
         self.crawler.signals.connect(self.engine_stopped, signal=signals.engine_stopped)
+        self.crawler.signals.connect(self.engine_started, signals.engine_started)
 
-        # Signals monitoring. Useful...
-        crawler.signals.connect(self.engine_started, signals.engine_started)
-
-        self.prometheus_default_labels = self.crawler.settings.getdict('PROMETHEUS_DEFAULT_LABELS', {})
-        if 'instance' not in self.prometheus_default_labels:
-            try:
-                self.prometheus_default_labels['instance'] = socket.gethostname()
-            except:
-                self.prometheus_default_labels['instance'] = ""
-
-        # Init all superclasses
         statscollectors.StatsCollector.__init__(self, crawler=crawler, **kwargs)
         ScrapyPrometheusWebServiceMixin.__init__(self, crawler=crawler, **kwargs)
 
+        self.registry=self.get_registry()
+
+        self.prometheus_default_labels = self.crawler.settings.getdict('PROMETHEUS_DEFAULT_LABELS', {})
+        self.prometheus_default_labels.setdefault('instance', socket.gethostname() or "")
+
+        self.metric_prefix = self.crawler.settings.get('PROMETHEUS_METRIC_PREFIX', 'scrapy_prometheus')
+        self.pushgateway=self.crawler.settings.get('PUSHGATEWAY', '127.0.0.1:9091')
+        self.pushgateway_method=self.crawler.settings.get('PUSHGATEWAY_PUSH_METHOD', 'POST')
+        self.pushgateway_timeout=self.crawler.settings.get('PUSHGATEWAY_PUSH_TIMEOUT', 5)
+        self.pushgatewayjob=self.crawler.settings.get('PUSHGATEWAY_JOB', 'pushgateway_scrapy')
 
     @classmethod
     def from_crawler(cls, crawler):
         stats = cls(crawler)
-        # Replace the default crawler.stats object with this, so that we collect
-        # metrics in Prometheus as well as a built-in collector.
         crawler.stats = stats
         return stats
 
-    def forced_spider(self, spider):
-        """
-        Force this spider to be used when writing metrics.
-
-        :param scrapy.spider.Spider spider:
-        """
-        self._forced_spider = spider
-
-    def get_registry(self, spider):
-        """
-        Return CollectorRegistry associated with spider. To get default CollectorRegistry, pass None.
-
-        :param scrapy.spider.Spider spider:
-        :rtype: prometheus_client.CollectorRegistry
-        """
-        # Hosting a Prometheus endpoint doesn't work well with multiple registries.
-        # So I think the spider name should just go into the metric label instead.
-
-        # reg_name = getattr(spider, 'name', "default")
-        reg_name = "default"
-        return self.registries[reg_name]
-
-    # noinspection PyProtectedMember
-    def get_metric(self, key, metric_type, spider=None, labels={}):
-        prefix = self.crawler.settings.get('PROMETHEUS_METRIC_PREFIX', 'scrapy_prometheus')
-        name = '%s_%s' % (prefix, key.replace('/', '_'))
-
-        registry = self.get_registry(spider)
-        _labels = labels if labels else self.prometheus_default_labels
-
+    def get_metric(self, key, value, spider=None, labels=None, metric_type=METRIC_GAUGE):
+        if not isinstance(value, (int, float)):
+            return None
+        
+        name = f"{self.metric_prefix}_{key.replace('/', '_')}"
+        registry = self.get_registry()
+        labels = labels or self.get_labels(spider)
+        
         if name not in registry._names_to_collectors:
-            metric, created = metric_type(name, key, _labels, registry=registry), True
+            metric = metric_type(name, key, labels, registry=registry)
         else:
-            metric, created = registry._names_to_collectors[name], False
+            metric = registry._names_to_collectors[name]
+        
+        return metric.labels(**labels)
 
-        return metric, created
-    
-    def _get_metric(self, key, value, spider=None, labels={}, metric_type=METRIC_GAUGE):
-        if isinstance(value, (int, float)):
-            if not labels:
-                labels = self.get_labels(spider)
-            metric, _ = self.get_metric(key, metric_type, spider=spider, labels=labels)
-            if metric:
-                specific_metric = metric.labels(**labels)
-                return specific_metric
-
-
-    @_forced_spider
-    def set_value(self, key, value, spider=None, labels={}):
-        super(PrometheusStatsCollector, self).set_value(key, value, spider)
-        metric = self._get_metric(key, value, spider=spider, labels=labels, metric_type=METRIC_GAUGE)
-        if metric:
+    def set_value(self, key, value, spider=None, labels=None):
+        super().set_value(key, value, spider)
+        if metric := self.get_metric(key, value, spider, labels, METRIC_GAUGE):
             metric.set(value)
 
-    @_forced_spider
-    def inc_value(self, key, count=1, start=0, spider=None, labels={}):
-        super(PrometheusStatsCollector, self).inc_value(key, count, start, spider)
-        metric = self._get_metric(key, start, spider=spider, labels=labels, metric_type=METRIC_COUNTER)
-        if metric:
+    def inc_value(self, key, count=1, start=0, spider=None, labels=None):
+        super().inc_value(key, count, start, spider)
+        if metric := self.get_metric(key, start, spider, labels, METRIC_COUNTER):
             metric.inc(count)
 
-    @_forced_spider
-    def max_value(self, key, value, spider=None, labels={}):
-        super(PrometheusStatsCollector, self).max_value(key, value, spider)
-        metric = self._get_metric(key, value, spider=spider, labels=labels, metric_type=METRIC_GAUGE)
-        if metric:
+    def max_value(self, key, value, spider=None, labels=None):
+        super().max_value(key, value, spider)
+        if metric := self.get_metric(key, value, spider, labels, METRIC_GAUGE):
             metric.set(max(metric._value.get(), value))
 
-    @_forced_spider
-    def min_value(self, key, value, spider=None, labels={}):
-        super(PrometheusStatsCollector, self).min_value(key, value, spider)
-        metric = self._get_metric(key, value, spider=spider, labels=labels, metric_type=METRIC_GAUGE)
-        if metric:
-            metric._value.set(min(metric._value.get(), value))
+    def min_value(self, key, value, spider=None, labels=None):
+        super().min_value(key, value, spider)
+        if metric := self.get_metric(key, value, spider, labels, METRIC_GAUGE):
+            metric.set(min(metric._value.get(), value))
 
     def get_labels(self, spider=None):
-        labels = {
-            'spider': spider.name if spider else '',
-            'job_id': spider.job_id if spider else ''
-        }
+        labels = {'spider': spider.name if spider else 'default', 'job_id': getattr(spider, 'job_id', '')}
         labels.update(self.prometheus_default_labels)
         return labels
 
     def _persist_stats(self, stats, spider=None):
-        super(PrometheusStatsCollector, self)._persist_stats(stats, spider)
-
-        # if spider and spider.name not in self.registries:
-        #     spider.logger.warning('%s spider not found in collector registries', spider.name)
-        #     return
-
+        super()._persist_stats(stats, spider)
         try:
             push_to_gateway(
-                pushgateway=self.crawler.settings.get('PROMETHEUS_PUSHGATEWAY', '127.0.0.1:9091'),
-                registry=self.registries["default"],
-                method=self.crawler.settings.get('PROMETHEUS_PUSH_METHOD', 'POST'),
-                timeout=self.crawler.settings.get('PROMETHEUS_PUSH_TIMEOUT', 5),
-                job=self.crawler.settings.get('PROMETHEUS_JOB', 'scrapy'),
+                pushgateway=self.pushgateway,
+                registry=self.registry,
+                method=self.pushgateway_method,
+                timeout=self.pushgateway_timeout,
+                job=self.pushgatewayjob,
                 grouping_key=self.get_labels(spider)
             )
-        except:
+        except Exception:
             if spider:
-                spider.logger.exception('Failed to push "%s" spider metrics to pushgateway', spider.name)
+                spider.logger.exception(f'Failed to push "{spider.name}" spider metrics to pushgateway')
         else:
             if spider:
-                spider.logger.info('Pushed "%s" spider metrics to pushgateway', spider.name)
+                spider.logger.info(f'Pushed "{spider.name}" spider metrics to pushgateway')
 
     def engine_started(self):
         self._start_prometheus_endpoint()
@@ -184,6 +101,7 @@ class PrometheusStatsCollector(ScrapyPrometheusWebServiceMixin, statscollectors.
         self._persist_stats(self._stats, None)
         self._stop_prometheus_endpoint()
 
+    # Default Metrics Handling
     def spider_opened(self, spider):
         if self.crawler.settings.getbool('PROMETHEUS_DEFAULT_METRICS', True):
             self.inc_value("spider_opened", spider=spider)
--- scrapy_prometheus/scrapy_prometheus_endpoint.py
+++ scrapy_prometheus/scrapy_prometheus_endpoint.py
@@ -1,66 +1,67 @@
 import logging
+from collections import defaultdict
 
 from prometheus_client.twisted import MetricsResource
-from prometheus_client import Counter, Summary, Gauge
+from prometheus_client import CollectorRegistry
+from twisted.web import resource
 from twisted.web.server import Site
-from twisted.web import server, resource
-from twisted.internet import task
 from scrapy.exceptions import NotConfigured
 from scrapy.utils.reactor import listen_tcp, install_reactor
-
 from scrapy import signals
 
 logger = logging.getLogger(__name__)
 
-
 class ScrapyPrometheusWebServiceMixin(Site):
     """
-    This class definition handles all of the hosting side of things.
+    Handles hosting and metrics exposure for Prometheus in Scrapy.
     """
     install_reactor("twisted.internet.asyncioreactor.AsyncioSelectorReactor")
 
     def __init__(self, crawler, **kwargs):
-        # Collect locals
-        self.tasks = []
         self.crawler = crawler
-        self.stats = self # The singularity cometh
+        self.tasks = []
+        self.stats = self
+        self.registries = defaultdict(CollectorRegistry)
 
-        # Add signals hooks
+        # Connect Scrapy signals
         crawler.signals.connect(self.spider_opened, signals.spider_opened)
         crawler.signals.connect(self.spider_closed, signals.spider_closed)
         crawler.signals.connect(self.item_scraped, signals.item_scraped)
         crawler.signals.connect(self.item_dropped, signals.item_dropped)
-        crawler.signals.connect(self.response_received,
-                                signals.response_received)
+        crawler.signals.connect(self.response_received, signals.response_received)
 
-        # Set prometheus settings
-        self.name = crawler.settings.get('BOT_NAME')
-        self.port = crawler.settings.get('PROMETHEUS_PORT', [9410])
-        self.host = crawler.settings.get('PROMETHEUS_HOST', '0.0.0.0')
-        self.path = crawler.settings.get('PROMETHEUS_PATH', 'metrics')
-        self.interval = crawler.settings.get('PROMETHEUS_UPDATE_INTERVAL', 30)
+        # Prometheus settings
+        settings = crawler.settings
+        self.prometheus_port = int(settings.get('PROMETHEUS_PORT', '9410'))
+        self.prometheus_host = settings.get('PROMETHEUS_HOST', '0.0.0.0')
+        self.prometheus_path = settings.get('PROMETHEUS_PATH', 'metrics')
+        self.prometheus_interval = settings.get('PROMETHEUS_UPDATE_INTERVAL', 30)
+        self.prometheus_endpoint_enabled = settings.getbool('PROMETHEUS_ENDPOINT_ENABLED', True)
+        self.reg_name = settings.get("DEFAULT_REGISTRY", "scrapy_prometheus")
+
         self._start_server()
 
+    def get_registry(self):
+        return self.registries[self.reg_name]
 
     def _start_server(self):
-        if self.crawler.settings.getbool('PROMETHEUS_ENDPOINT_ENABLED', True):                                         
-            root = resource.Resource()
-            self.prometheus = None
+        if not self.prometheus_endpoint_enabled:
+            return
 
-            registry = self.get_registry(spider=None)
-            self._prom_metrics_resource = MetricsResource(registry=registry)
-            root.putChild(self.path.encode('utf-8'), self._prom_metrics_resource)
-            server.Site.__init__(self, root)
+        root = resource.Resource()
+        registry = self.get_registry()
+        self._prom_metrics_resource = MetricsResource(registry=registry)
+        root.putChild(self.prometheus_path.encode('utf-8'), self._prom_metrics_resource)
+        Site.__init__(self, root)
 
     def _start_prometheus_endpoint(self):
-        if self.crawler.settings.getbool('PROMETHEUS_ENDPOINT_ENABLED', True):
-            self.prometheus = listen_tcp(self.port, self.host, self)
+        if self.prometheus_endpoint_enabled:
+            self.prometheus = listen_tcp(self.prometheus_port, self.prometheus_host, self)
 
     def _stop_prometheus_endpoint(self):
-        # Stop all periodic tasks
-        for tsk in self.tasks:
-            if tsk.running:
-                tsk.stop()
-
-        # Stop metrics exporting
-        self.prometheus.stopListening()
+        for task in self.tasks:
+            if task.running:
+                task.stop()
+        
+        if hasattr(self, 'prometheus') and self.prometheus:
+            self.prometheus.stopListening()
